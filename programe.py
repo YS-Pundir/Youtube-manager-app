aa92bc609988b1886ae6e5f2c5c9f6c18997998b
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-12 20:56:21 +0100

    adding a main interface programe

--- programe.py
+++ programe.py
@@ -1,1 +1,20 @@
+def list_all_videos(videos):
+    
 
+
+while True:
+    print("-"*50)
+    print("Youtube Manager")
+    print("-"*50)
+    print()
+    print("Choose an option")
+    print("1. List all Youtube Video")
+    print("2. Add a Youtube video ")
+    print("3. Update a youtube vedio details")
+    print("4. Delete a youtube video")
+    print("5. Exit the app")
+    choice=int(input("Enter the choice please : ",))
+
+    match choice:
+        case 1:
+            list_all_videos(video)
