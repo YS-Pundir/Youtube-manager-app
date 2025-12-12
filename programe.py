a053f342eb57783ac7044508e44e35af3d23634b
YS-Pundir <yuvrajpundir2005@gmail.com>
2025-12-12 21:25:54 +0100

    adding function fo load data

--- programe.py
+++ programe.py
@@ -1,20 +1,64 @@
-def list_all_videos(videos):
+import json
+
+def load_data():
+    try :
+        with open("youtube.txt","r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
     
 
+    
+        
+            
+    
+def list_all_videos(videos):
+    pass
 
-while True:
-    print("-"*50)
-    print("Youtube Manager")
-    print("-"*50)
-    print()
-    print("Choose an option")
-    print("1. List all Youtube Video")
-    print("2. Add a Youtube video ")
-    print("3. Update a youtube vedio details")
-    print("4. Delete a youtube video")
-    print("5. Exit the app")
-    choice=int(input("Enter the choice please : ",))
+def add_video(videos):
+    pass
 
-    match choice:
-        case 1:
-            list_all_videos(video)
+def update_videos(videos):
+    pass
+def delete_video(videos):
+    pass
+def exit_app(videos):
+    pass
+                  
+
+
+
+
+videos=[]
+def main():
+    while True:
+        print("-"*50)
+        print("Youtube Manager")
+        print("-"*50)
+        print()
+        print("Choose an option")
+        print("1. List all Youtube Video")
+        print("2. Add a Youtube video ")
+        print("3. Update a youtube vedio details")
+        print("4. Delete a youtube video")
+        print("5. Exit the app")
+        choice=int(input("Enter the choice please : ",))
+
+        match choice:
+            case 1:
+                list_all_videos(videos)
+            case 2:
+                add_video(videos)
+            case 3:
+                update_videos(videos)
+            case 4:
+                delete_video(videos)
+            case 5 :
+                exit_app(videos)
+
+if __name__=="__main__":#
+    main()
+    
+    
+        
+        
